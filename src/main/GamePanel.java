9283d8068d5bfa91f51312b148d322a4e1321355
unknown <alexander15clarke@gmail.com>
2023-02-05 18:05:13 -0600

    initial commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,88 @@
+package main;
+
+import entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    // Set player's position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel () {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while(gameThread != null) {
+
+
+
+            update();
+
+            repaint();
+
+
+
+
+            // Handle time for the game loop so 60 FPS
+            double remainingTime = nextDrawTime - System.nanoTime();
+            remainingTime = remainingTime / 1000000;
+            try {
+                if (remainingTime < 0) {
+                    remainingTime = 0;
+                }
+                Thread.sleep((long)remainingTime);
+
+                nextDrawTime += drawInterval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            // --------------------------------- //
+        }
+    }
+    public void update() {
+        player.update();
+    }
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        player.draw(g2);
+
+        g2.dispose();
+    }
+}
